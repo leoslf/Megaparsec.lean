88b706ab5cf726ac851300dec7fefe7c2029c600
Leo Sin <leo.sin@my.cityu.edu.hk>
2025-08-12 20:56:39 -0400

    build: revert to v4.21.0

--- lakefile.lean
+++ lakefile.lean
@@ -9,11 +9,8 @@ lean_lib Megaparsec
 require LSpec from git
   "https://github.com/argumentcomputer/LSpec" @ "24cceb69c20fadca0fd3acabe39fa9270dfb47e6"
 
-require YatimaStdLib from git
-  "https://github.com/leoslf/YatimaStdLib.lean" @ "c6e0cf01dc8c16654044c166c69b777043a8d7e0"
-
 require Straume from git
-  "https://github.com/leoslf/straume" @ "5a28142a97ef9dc07f892b87757d6eb6a2e11844"
+  "https://github.com/leoslf/straume" @ "52056899cc811bc1b3ccdc9c217fc815c3c8b95d"
 
 @[default_target]
 lean_exe megaparsec where
